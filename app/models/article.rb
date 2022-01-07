6ad3d6a5351fd11e631409ceebbb1456d2673cb9
CarlosZgierski <carloszgierski@outlook.com>
2022-01-07 16:54:39 -0300

    MUltiple changes to multiple Scripts

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+  validates :title, presence: true, uniqueness: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
+  before_validation :remove_deplicate_spaces
+
+  def remove_deplicate_spaces
+    self.title.to_s.squish!
+    self.description.to_s.squish!
+  end
+
+  # create update (save) delete
 end
